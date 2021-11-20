491a3b148d09d7142ee1eae512b8ee46f1f86191
陈征 <1007324849@qq.com>
2021-11-20 16:45:08 +0800

    select 操作

--- src/decorator/select.py
+++ src/decorator/select.py
@@ -1,2 +1,14 @@
-def select():
-    pass
+import functools
+from typing import Callable
+
+
+def select(sql: str) -> Callable:
+    def deco(fun: Callable):
+        @functools.wraps(fun)
+        def wrapper(*args, **kwargs):
+
+            fun(*args, **kwargs)
+
+        return wrapper
+
+    return deco
