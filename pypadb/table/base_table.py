03160e5f15b02331ce6ad7c0dccc2249edcbe056
ChenZheng <1007324849@qq.com>
2022-01-25 19:34:10 +0800

    feat: 增删改功能

--- pypadb/table/base_table.py
+++ pypadb/table/base_table.py
@@ -2,7 +2,16 @@ from typing import Any, Union
 
 from utils.conditions import Limit, Like
 from utils.enums import QueryModeEnum
-from utils.query_util import query
+from utils.query_util import query, execute
+
+
+def parse_sql(sql: str, conditions: dict, limit: Limit = None) -> str:
+    if conditions:
+        sql += ' where'
+        for key in conditions:
+            sql += f' {key}=%({key})s and'
+    sql = sql.rstrip(' and')
+    return sql + str(limit) if limit else sql
 
 
 class BaseTable:
@@ -18,11 +27,11 @@ class BaseTable:
         self.base_select_sql = f'select * from {name}'
 
     def select_one(self, extra=None, **kwargs):
-        res = query(self.__parse_sql(kwargs), kwargs, self.data_type, QueryModeEnum.One)
+        res = query(parse_sql(self.base_select_sql, kwargs), kwargs, self.data_type, QueryModeEnum.One)
         return extra(res) if extra else res
 
     def select_many(self, limit: Limit = None, extra=None, **kwargs) -> list:
-        res = query(self.__parse_sql(kwargs, limit), kwargs, self.data_type, QueryModeEnum.Many)
+        res = query(parse_sql(self.base_select_sql, kwargs, limit), kwargs, self.data_type, QueryModeEnum.Many)
         return extra(res) if extra else res
 
     def select_like(self,
@@ -44,23 +53,54 @@ class BaseTable:
         res = query(sql + str(limit) if limit else sql, data_dict, self.data_type, QueryModeEnum.Many)
         return extra(res) if extra else res
 
-    def __parse_sql(self, conditions: dict, limit: Limit = None) -> str:
-        sql: str = self.base_select_sql
-        if conditions:
-            sql += ' where'
-            for key in conditions:
-                sql += f' {key}=%({key})s and'
-        sql = sql.rstrip(' and')
-        return sql + str(limit) if limit else sql
+    def update(self, entity, **kwargs):
+        if not kwargs:
+            return
+        sql = f'update {self.name}'
+        data_dict = entity.dict()
+        if entity:
+            sql += ' set'
+            for key in data_dict:
+                if data_dict[key] or data_dict[key] == 0:
+                    sql += f' {key}=%({key})s,'
+            sql = sql.rstrip(',')
+        sql = parse_sql(sql, kwargs)
+        return execute(sql, {**data_dict, **kwargs})
 
-    def update_by_primary(self):
-        pass
-
-    def delete_by_primary(self):
-        pass
+    def delete(self, **kwargs):
+        if not kwargs:
+            return
+        sql = f'delete from {self.name}'
+        sql = parse_sql(sql, kwargs)
+        return execute(sql, kwargs)
 
     def insert(self, data):
-        pass
+        sql = f'insert into {self.name}('
+        tmp = '('
+        data_dict: dict = data.dict()
+        for key in data_dict:
+            if data_dict[key] or data_dict[key] == 0:
+                sql += f'{key}, '
+                tmp += f'%({key})s, '
+        tmp = tmp.rstrip(', ') + ')'
+        sql = sql.rstrip(', ') + ') value' + tmp
+        return execute(sql, data_dict)
 
     def insert_batch(self, data: list):
-        pass
+        sql = f'insert into {self.name}('
+        data_dict: dict = data[0].dict()
+        for key in data_dict:
+            if data_dict[key] or data_dict[key] == 0:
+                sql += f' {key}, '
+        sql = sql.rstrip(', ') + ') values '
+        data_list_tmp: list[dict] = [i.dict() for i in data]
+        data_dict = {}
+        for i, v in enumerate(data_list_tmp):
+            sql += '('
+            for k in v:
+                data_dict[f'{k}{i}'] = v[k]
+                sql += f'%({k}{i})s, '
+            sql = sql.rstrip(', ') + '), '
+        sql = sql.rstrip(', ')
+        print(sql, data_dict)
+        return execute(sql, data_dict)
--- pypadb/utils/query_util.py
+++ pypadb/utils/query_util.py
@@ -5,21 +5,26 @@ from utils.enums import QueryModeEnum
 
 
 def query(sql: str, kwargs: dict, data_type: Any, model: QueryModeEnum):
+    result_set: list = execute(sql, kwargs, connection_pool.cursor_type())
+    if result_set is None:
+        return None
+
+    if model == QueryModeEnum.One:
+        return data_type(**result_set[0])
+    elif model == QueryModeEnum.Many:
+        return [data_type(**i) for i in result_set]
+
+
+def execute(sql: str, kwargs: dict = None, cursor_type=None) -> list:
     conn: Any
     cur: Any
     try:
         conn = connection_pool.connection()
-        cur = conn.cursor(connection_pool.cursor_type())
+        cur = conn.cursor(cursor_type) if cursor_type else conn.cursor()
         cur.execute(sql, kwargs)
 
-        first_data = cur.fetchone()
-        if first_data is None:
-            return None
-
-        if model == QueryModeEnum.One:
-            return data_type(**first_data)
-        elif model == QueryModeEnum.Many:
-            return [data_type(**first_data), *[data_type(**i) for i in cur.fetchall()]]
+        return cur.fetchall()
     finally:
+        conn.commit()
         conn.close()
         cur.close()
