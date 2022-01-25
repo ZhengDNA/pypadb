ebf0b56fee7abb6d8c178618020545777ddeb295
ChenZheng <1007324849@qq.com>
2022-01-25 17:27:47 +0800

    perf: 优化性能

--- pypadb/conf/table_configurer.py
+++ pypadb/conf/table_configurer.py
@@ -4,31 +4,19 @@ from table.base_table import BaseTable
 class TableConfigurer:
     __slots__ = ['tables']
 
-    tables: list[BaseTable]
-
-    def __init__(self, **kwargs):
-        self.tables = []
-        if kwargs:
-            for key in kwargs:
-                self.tables.append(BaseTable(key, kwargs.get(key)))
+    tables: dict[str, BaseTable]
 
     def __getattr__(self, item: str) -> BaseTable:
         return self[item]
 
     def __getitem__(self, item) -> BaseTable:
-        if isinstance(item, int):
-            return self.tables[item]
-        if isinstance(item, str):
-            for table in self.tables:
-                if table.name == item:
-                    return table
-        return None
+        return tables[item]
 
     def init_tables(self, **kwargs):
-        self.tables = []
+        self.tables = {}
         if kwargs:
             for key in kwargs:
-                self.tables.append(BaseTable(key, kwargs.get(key)))
+                self.tables[key] = BaseTable(key, kwargs.get(key))
 
 
 tables: TableConfigurer = TableConfigurer()
