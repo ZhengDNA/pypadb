191d46f3106bf8f49c7ee4704ed520db6a3d9ea9
ChenZheng <1007324849@qq.com>
2022-01-29 02:32:53 +0800

    feat: 插入装饰器支持 list 批量插入

--- pypadb/decorator/insert.py
+++ pypadb/decorator/insert.py
@@ -8,21 +8,30 @@ from utils.query_util import execute
 def insert(sql: str) -> Callable:
     arg_list = [s.strip() for s in re.findall(r'[(](.*?)[)]', sql)[0].split(',')]
     sql += ' values'
-    template_sql = '('
-    for arg in arg_list:
-        template_sql += f'%({arg})s,'
-    template_sql = template_sql.rstrip(',') + ')'
 
     def deco(fun):
         @functools.wraps(fun)
         def wrapper(data):
             in_sql = sql
-            if isinstance(data, list):
-                for i in data:
-                    in_sql += (template_sql % i.dict()) + ','
-                _, row_id = execute(in_sql.rstrip(','))
+            if not isinstance(data, list):
+                template_sql = '('
+                for arg in arg_list:
+                    template_sql += f'%({arg})s,'
+                template_sql = template_sql.rstrip(',') + '),'
+                _, row_id = execute(in_sql + template_sql, data.dict())
                 return row_id
-            _, row_id = execute(in_sql + template_sql, data.dict())
+
+            query_dict: dict = {}
+            for i, v in enumerate(data):
+                data_dict: dict = v.dict()
+                query_dict = {**query_dict, **{f'{arg}{i}': data_dict.get(arg) for arg in arg_list}}
+                in_sql += '('
+                for item in arg_list:
+                    in_sql += f'%({item}{i})s,'
+                in_sql = in_sql.rstrip(',') + '),'
+
+            print(in_sql)
+            _, row_id = execute(in_sql.rstrip(','), query_dict)
             return row_id
 
         return wrapper
