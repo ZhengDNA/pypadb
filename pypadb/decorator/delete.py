849193fdeae4ce910ebe55445598454bbe339bbf
ChenZheng <1007324849@qq.com>
2022-01-29 03:16:49 +0800

    feat: 删除操作的装饰器

--- pypadb/decorator/delete.py
+++ pypadb/decorator/delete.py
@@ -1,2 +1,16 @@
-def delete():
-    pass
+import functools
+
+from utils import inspect_util
+from utils.query_util import execute
+
+
+def delete(sql: str):
+    def deco(fun):
+        @functools.wraps(fun)
+        def wrapper(*args):
+            lines, _ = execute(sql, dict(zip([a.name for a in inspect_util.arg_list(fun)], args)))
+            return lines
+
+        return wrapper
+
+    return deco
