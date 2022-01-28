3a3eb98827ab93dd5a16dd59d4b2b05c7cb75662
ChenZheng <1007324849@qq.com>
2022-01-29 00:38:15 +0800

    feat: 插入的装饰器

--- pypadb/decorator/insert.py
+++ pypadb/decorator/insert.py
@@ -1,2 +1,17 @@
-def insert():
-    pass
+import functools
+from typing import Callable
+
+from utils import inspect_util
+from utils.query_util import execute
+
+
+def insert(sql: str) -> Callable:
+    def deco(fun):
+        @functools.wraps(fun)
+        def wrapper(data):
+            _, row_id = execute(sql, data.dict())
+            return row_id
+
+        return wrapper
+
+    return deco
