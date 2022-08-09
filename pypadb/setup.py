8195789550d7b3b060b298c39a77226c44f249dd
ChenZheng <1007324849@qq.com>
2022-08-10 04:14:39 +0800

    fix: 特定表名下sql解析不正确

--- pypadb/setup.py
+++ pypadb/setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name="pypadb",
-    version="0.1.7",
+    version="0.1.8",
     description='Interaction with database in a simple way',
     author="ChenzDNA",
     author_email='1007324849@qq.com',
